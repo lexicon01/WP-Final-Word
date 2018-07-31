19f5549bd11d59c66bc1b52434f7d987b23aa183
Alex Roman <ialex.roman@icloud.com>
2018-07-31 17:24:50 -0400

    added some files

--- wp-content/themes/humescores/gulpfile.js
+++ wp-content/themes/humescores/gulpfile.js
@@ -1,29 +1,24 @@
-var themename = 'theme/humescores';
+var themename = 'humescores';
 
 var gulp = require('gulp'),
 	// Prepare and optimize code etc
 	autoprefixer = require('autoprefixer'),
 	browserSync = require('browser-sync').create(),
-	image = require('gulp-image'),
-	jshint = require('gulp-jshint'),
 	postcss = require('gulp-postcss'),
 	sass = require('gulp-sass'),
 	sourcemaps = require('gulp-sourcemaps'),
 
 	// Only work with new or updated files
-	newer = require('gulp-newer'),
+
 
 	// Name of working theme folder
 	root = '../' + themename + '/',
-	scss = root + 'sass/',
-	js = root + 'js/',
-	img = root + 'images/',
-	languages = root + 'languages/';
+	scss = root + 'sass/';
 
 
 // CSS via Sass and Autoprefixer
 gulp.task('css', function() {
-	return gulp.src(scss + '{style.scss,rtl.scss}')
+	return gulp.src('sass/**/*.scss')
 	.pipe(sourcemaps.init())
 	.pipe(sass({
 		outputStyle: 'expanded', 
@@ -34,24 +29,10 @@ gulp.task('css', function() {
 		autoprefixer('last 2 versions', '> 1%')
 	]))
 	.pipe(sourcemaps.write(scss + 'maps'))
-	.pipe(gulp.dest(root));
+	.pipe(gulp.dest('./'));
 });
 
-// Optimize images through gulp-image
-gulp.task('images', function() {
-	return gulp.src(img + 'RAW/**/*.{jpg,JPG,png}')
-	.pipe(newer(img))
-	.pipe(image())
-	.pipe(gulp.dest(img));
-});
 
-// JavaScript
-gulp.task('javascript', function() {
-	return gulp.src([js + '*.js'])
-	.pipe(jshint())
-	.pipe(jshint.reporter('default'))
-	.pipe(gulp.dest(js));
-});
 
 
 // Watch everything
@@ -61,10 +42,7 @@ gulp.task('watch', function() {
 		proxy: 'localhost/wordpress/htdocs/wp-content/theme/humescores',
 		port: 8080
 	});
-	gulp.watch([root + '**/*.css', root + '**/*.scss' ], ['css']);
-	gulp.watch(js + '**/*.js', ['javascript']);
-	gulp.watch(img + 'RAW/**/*.{jpg,JPG,png}', ['images']);
-	gulp.watch(root + '**/*').on('change', browserSync.reload);
+	gulp.watch('sass/**/*.scss', ['css']).on('change', browserSync.reload);
 });
 
 
