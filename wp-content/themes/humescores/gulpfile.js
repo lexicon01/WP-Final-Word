e9a5e74bda974e1898006272dce28ac0a9e42e9a
Alex Roman <ialex.roman@icloud.com>
2018-08-01 18:02:27 -0400

    added some changes hopfully this works

--- wp-content/themes/humescores/gulpfile.js
+++ wp-content/themes/humescores/gulpfile.js
@@ -1,24 +1,31 @@
-var themename = 'humescores';
+const themename = 'humescores';
 
-var gulp = require('gulp'),
+const gulp = require('gulp');
 	// Prepare and optimize code etc
-	autoprefixer = require('autoprefixer'),
-	browserSync = require('browser-sync').create(),
-	postcss = require('gulp-postcss'),
-	sass = require('gulp-sass'),
-	sourcemaps = require('gulp-sourcemaps'),
+const	autoprefixer = require('autoprefixer');
+const	browserSync = require('browser-sync').create();
+const	image = require('gulp-image');
+const	jshint = require('gulp-jshint');
+const	postcss = require('gulp-postcss');
+const	sass = require('gulp-sass');
+const	sourcemaps = require('gulp-sourcemaps');
 
 	// Only work with new or updated files
+const	newer = require('gulp-newer');
+	
+		// Name of working theme folder
+const root = '../' + themename + '/';
+const		scss = root + 'sass/'
+const		js = root + 'js/';
+const		img = root + 'images/';
+const		languages = root + 'languages/';
 
 
-	// Name of working theme folder
-	root = '../' + themename + '/',
-	scss = root + 'sass/';
 
 
 // CSS via Sass and Autoprefixer
 gulp.task('css', function() {
-	return gulp.src('sass/**/*.scss')
+	return gulp.src( scss + '{style.scss,rtl.scss}')
 	.pipe(sourcemaps.init())
 	.pipe(sass({
 		outputStyle: 'expanded', 
@@ -28,23 +35,43 @@ gulp.task('css', function() {
 	.pipe(postcss([
 		autoprefixer('last 2 versions', '> 1%')
 	]))
-	.pipe(sourcemaps.write(scss + 'maps'))
-	.pipe(gulp.dest('./'));
+	.pipe(sourcemaps.write( scss + 'maps'))
+	.pipe(gulp.dest(root));
 });
 
+//Optimize images through gulp-image
+gulp.task('images', function() {
+	return gulp.src(img + 'RAW/**/*.{jpg,JPG,png}')
+	.pipe(newer(img))
+	.pipe(image())
+	.pipe(gulp.dest(img));
+});
 
+// JavaScript
+gulp.task('javascript', function() {
+	return gulp.src([js + '*.js'])
+	.pipe(jshint())
+	.pipe(jshint.reporter('default'))
+	.pipe(gulp.dest(js));
+});
 
 
 // Watch everything
 gulp.task('watch', function() {
 	browserSync.init({ 
 		open: 'external',
-		proxy: 'localhost/wordpress/htdocs/wp-content/theme/humescores',
+		proxy: 'localhost/wordpress/humescores',
 		port: 8080
 	});
-	gulp.watch('sass/**/*.scss', ['css']).on('change', browserSync.reload);
+	gulp.watch([root + '**/*.css', root + '**/*.scss' ], ['css']).on('change', browserSync.reload);
+	
+	gulp.watch(js + '**/*.js', ['javascript']).on('change', browserSync.reload);
+	
+	gulp.watch(img + 'RAW/**/*.{jpg,JPG,png}', ['images']).on('change', browserSync.reload);
+	
+
 });
 
 
 // Default task (runs at initiation: gulp --verbose)
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch','css','javascript','images']);
