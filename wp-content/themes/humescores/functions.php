b80b1157f8afffa02e0eabf901c78fbf267cfd1b
Alex Roman <ialex.roman@icloud.com>
2018-08-10 18:35:26 -0400

    added new files

--- wp-content/themes/humescores/functions.php
+++ wp-content/themes/humescores/functions.php
@@ -44,7 +44,7 @@ if ( ! function_exists( 'humescores_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'humescores' ),
+			'menu-1' => esc_html__( 'header', 'humescores' ),
 		) );
 
 		/*
