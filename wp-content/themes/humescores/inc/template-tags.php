a2237996c5371bab5d7980c9dfae689a3d075ff0
Alex Roman <ialex.roman@icloud.com>
2018-08-16 21:03:10 -0400

    getting that green status

--- wp-content/themes/humescores/inc/template-tags.php
+++ wp-content/themes/humescores/inc/template-tags.php
@@ -58,13 +58,7 @@ if ( ! function_exists( 'humescores_entry_footer' ) ) :
 	function humescores_entry_footer() {
 		// Hide category and tag text for pages.
 		if ( 'post' === get_post_type() ) {
-			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( esc_html__( ', ', 'humescores' ) );
-			if ( $categories_list ) {
-				/* translators: 1: list of categories. */
-				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'humescores' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-			}
-
+		
 			/* translators: used between list items, there is a space after the comma */
 			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'humescores' ) );
 			if ( $tags_list ) {
@@ -79,7 +73,7 @@ if ( ! function_exists( 'humescores_entry_footer' ) ) :
 				sprintf(
 					wp_kses(
 						/* translators: %s: post title */
-						__( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'humescores' ),
+						__( ' Leave a Comment<span class="screen-reader-text"> on %s</span>', 'humescores' ),
 						array(
 							'span' => array(
 								'class' => array(),
@@ -105,7 +99,7 @@ if ( ! function_exists( 'humescores_entry_footer' ) ) :
 				),
 				get_the_title()
 			),
-			'<span class="edit-link">',
+			' <span class="edit-link">',
 			'</span>'
 		);
 	}
